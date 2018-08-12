a4f649a2bceb0b1378fed360559a709a417cf135
maharama <maramstratos@gmail.com>
2018-08-12 21:14:55 +0700

    markdown added, todo auth added

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # todo-not-so-fancy-server
-server for todo not so fancy
+Server for "todo not so fancy"
+
+## List API
+---
+
+**Public Route**
+
+| Route      | HTTP   |     Description    |
+|------------|--------|:------------------:|
+| /signup    | POST   | Sign up a user     |
+| /login     | POST   | Manual login       |
+| /fblogin   | POST   | Login via facebook |
+
+**Users Route**
+
+| Route      | HTTP   |     Description    |
+|------------|--------|:------------------:|
+| /users     | GET    | Get users          |
+| /users/:id | GET    | Get one user       |
+| /users     | POST   | Sign up a user     |
+| /users/:id | DELETE | Delete a user      |
+| /users/:id | PUT    | Update a user      |
--- routes/todos.js
+++ routes/todos.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var router = express.Router();
 var todoController = require('../controllers/todosController')
+const Authorization = require('../middlewares/authorization')
+const Authentication = require('../middlewares/authentication')
 
 /* GET todos listing. */
-router.post('/:userId', todoController.getTodo);
-router.post('/:userId/add', todoController.addTodo);
-router.delete('/delete', todoController.deleteTodo);
-router.put('/updateDone', todoController.updateDone);
-router.put('/updateNot', todoController.updateNot);
+router.post('/:userId', Authentication.authenticationRead, todoController.getTodo);
+router.post('/:userId/add', Authentication.authenticationRead, todoController.addTodo);
+router.delete('/delete', Authentication.authenticationRead, todoController.deleteTodo);
+router.put('/updateDone', Authentication.authenticationRead, todoController.updateDone);
+router.put('/updateNot', Authentication.authenticationRead, todoController.updateNot);
 
 module.exports = router;
